8f7c65d2c07beb0eeb53fd82e0773f640d9e0554
Leonardo Villalba <adg.leo.lv@gmail.com>
2022-07-13 23:04:26 -0500

    serializer updated

---
+++ src/main/java/com/latam/jala/lambda/InputTranscript.java
@@ -0,0 +1,13 @@
+package com.latam.jala.lambda;
+
+public class InputTranscript {
+    private String inputTranscript;
+
+    public String getInputTranscript() {
+        return inputTranscript;
+    }
+
+    public void setInputTranscript(String inputTranscript) {
+        this.inputTranscript = inputTranscript;
+    }
+}
